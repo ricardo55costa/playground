a615b168b235c58b967f923950c57cd482e833ed
ricardo55costa <ricardosilvacosta@macominho.pt>
2023-05-13 10:55:13 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ OpenAIPlayground.dpr
@@ -0,0 +1,29 @@
+﻿program OpenAIPlayground;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  OpenAIPG.Main in 'OpenAIPG.Main.pas' {FormMain},
+  OpenAIPG.Frame.Code in 'OpenAIPG.Frame.Code.pas' {FrameCode: TFrame},
+  OpenAIPG.Frame.Score in 'OpenAIPG.Frame.Score.pas' {FrameScore: TFrame},
+  OpenAI.API.Params in 'DelphiOpenAI\OpenAI.API.Params.pas',
+  OpenAI.API in 'DelphiOpenAI\OpenAI.API.pas',
+  OpenAI.Completions in 'DelphiOpenAI\OpenAI.Completions.pas',
+  OpenAI.Edits in 'DelphiOpenAI\OpenAI.Edits.pas',
+  OpenAI.Embeddings in 'DelphiOpenAI\OpenAI.Embeddings.pas',
+  OpenAI.Engines in 'DelphiOpenAI\OpenAI.Engines.pas',
+  OpenAI.Errors in 'DelphiOpenAI\OpenAI.Errors.pas',
+  OpenAI.Files in 'DelphiOpenAI\OpenAI.Files.pas',
+  OpenAI.FineTunes in 'DelphiOpenAI\OpenAI.FineTunes.pas',
+  OpenAI.Images in 'DelphiOpenAI\OpenAI.Images.pas',
+  OpenAI.Models in 'DelphiOpenAI\OpenAI.Models.pas',
+  OpenAI.Moderations in 'DelphiOpenAI\OpenAI.Moderations.pas',
+  OpenAI in 'DelphiOpenAI\OpenAI.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TFormMain, FormMain);
+  Application.Run;
+end.
---
+++ README.md
@@ -0,0 +1,5 @@
+# Delphi OpenAI Playground
+ 
+ API overview for working with OpenAI
+
+![Preview](https://github.com/HemulGM/DelphiOpenAIPlayground/blob/main/preview.png?raw=true)
